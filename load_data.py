6579100ba47c0beb7b00501770d4129554e70011
mwongeraE <evansmwongera@gmail.com>
2021-02-02 11:22:56 +0300

    load data

---
+++ load_data.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+from surprise import Dataset
+from surprise import Reader
+
+#Ratings dictionary
+ratings_dict = {
+    "item": [1, 2, 1, 2, 1, 2, 1, 2, 1],
+    "user": ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E'],
+    "rating": [1, 2, 2, 4, 2.5, 4, 4.5, 5, 3],
+}
+
+df = pd.DataFrame(ratings_dict)
+reader = Reader(rating_scale=(1, 5))
+
+# Loads Pandas dataframe
+data = Dataset.load_from_df(df[["user", "item", "rating"]], reader)
+# Loads the builtin Movielens data
+movielens = Dataset.load_builtin('ml-100k')
